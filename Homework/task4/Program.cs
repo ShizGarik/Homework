b61f8e0a0a8a0a144854d7d5adfafcb7cf5d4c33
Dmitry Goncharenko <d.goncharenko1@gmail.com>
2023-01-17 19:09:47 +0300

    task4

---
+++ Homework/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int i = 1;
+
+System.Console.WriteLine("Программа, показывает четные числа от 1 до N");
+
+Console.Write("Введите четное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (i <= a )
+{
+    if( i % 2 == 0)
+    {
+        System.Console.WriteLine(i);
+    }
+    i++;
+}
