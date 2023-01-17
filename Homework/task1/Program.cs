45da220f8bc5e11e4204d9be9c3b8652b4ce8a83
Dmitry Goncharenko <d.goncharenko1@gmail.com>
2023-01-17 16:58:44 +0300

    task1

---
+++ Homework/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Программа нахождения максимального числа и наименьшего");
+
+System.Console.Write("Введите первое число: ");
+int a = System.Convert.ToInt32( System.Console.ReadLine());
+
+System.Console.Write("Введите второе число: ");
+int b = System.Convert.ToInt32(System.Console.ReadLine());
+
+if(a > b)
+{
+    System.Console.WriteLine($"max {a} min {b}");
+}else if (a == b)
+{
+   System.Console.WriteLine($"Число {a} и число {b} равны");
+}else
+{
+    System.Console.WriteLine($"max {b} min {a}");
+}
