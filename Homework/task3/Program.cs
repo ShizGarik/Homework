b9041f2519fc51753a90c6e475d9395e8b8381aa
Dmitry Goncharenko <d.goncharenko1@gmail.com>
2023-01-17 18:27:49 +0300

    task3

---
+++ Homework/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа по проверке четности числа (делится ли оно на два без остатка)");
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ( a % 2 == 0 )
+{
+    System.Console.WriteLine($"Число {a} четное и делится на два");
+}else
+{
+    System.Console.WriteLine($"Число {a} является не четным");
+}
