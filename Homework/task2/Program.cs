ed899066ce6538e896dc09cecd19731c967cdbae
Dmitry Goncharenko <d.goncharenko1@gmail.com>
2023-01-17 17:32:46 +0300

    task2

---
+++ Homework/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Программа нахождения максимального из трех чисел");
+
+System.Console.Write("Введите первое число: ");
+int a = System.Convert.ToInt32( System.Console.ReadLine());
+
+System.Console.Write("Введите второе число: ");
+int b = System.Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("Введите третье число: ");
+int c = System.Convert.ToInt32(System.Console.ReadLine());
+
+if(a > b && a > c)
+{
+    System.Console.WriteLine($"max {a}");
+}else if (b > a && b > c)
+{
+   System.Console.WriteLine($"max {b}");
+}else
+{
+    System.Console.WriteLine($"max {c}");
+}
