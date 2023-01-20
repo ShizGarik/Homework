23fa42154caab65730ea7734c4a64d1d0d6b95de
Dmitry Goncharenko <d.goncharenko1@gmail.com>
2023-01-20 20:53:04 +0300

    task5

---
+++ Homework/task5/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+    
+    int number;
+
+    do
+    {
+        Console.Write("Введите трехзначное число: ");
+        Int32.TryParse(Console.ReadLine(), out number);
+
+    }while (number < 100 || number > 1000);
+
+    int twoNumber = number / 10;
+    int oneNumber = twoNumber % 10;
+    
+    Console.WriteLine($"Вторая цифра числа: {oneNumber}");
