1c9f35a676d7cc252116651490d4a0b237df3473
Dmitry Goncharenko <d.goncharenko1@gmail.com>
2023-01-24 20:03:35 +0300

    task8, task9, task10

---
+++ Homework/task8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+int number;
+
+System.Console.Write("Введите пятизначное число: ");
+Int32.TryParse(Console.ReadLine(), out number); // Не работает TryParse!!!!!!!!!!
+string str = $"{number}"; // преобразовал целое число в строку
+
+if (str[0] == str[4] && str[1] == str[3])
+{
+    System.Console.WriteLine($"{number} --> да");
+}else
+{
+    System.Console.WriteLine($"{number} --> нет");
+}
