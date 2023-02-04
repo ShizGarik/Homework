01fcd7de4734b9c214265b005b264332f0097bdc
Dmitry Goncharenko <d.goncharenko1@gmail.com>
2023-02-04 12:40:59 +0300

    task17, task18

---
+++ Homework/task17/Program.cs
@@ -0,0 +1,45 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Input(string message)
+{
+    int number;
+    System.Console.Write(message);
+
+    try
+    {
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    catch
+    {
+        System.Console.WriteLine("Не то");
+        number = Input(message);
+    }
+
+    // number = number > 0 ? number : Input(message);
+    return number;
+}
+
+int count = 0;
+
+int a = Input("Введите размер массива: ");
+
+int[] arr = new int[a];
+
+for (int i = 0; i < arr.Length; i++) // цикл для ввода элемента массива
+{
+    System.Console.Write($"\n Введите элемент массива {i}: ");
+    arr[i] = Input(" ");
+}
+Console.WriteLine("[{0}]", string.Join(", ", arr));
+
+for (int i = 0; i < arr.Length; i++) // цикл для подсчета чисел больше нуля
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine("Чисел больше нуля: " + count);
---
+++ Homework/task18/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Input(string message)
+{
+    int number;
+    System.Console.Write(message);
+
+    try
+    {
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    catch
+    {
+        System.Console.WriteLine("Не то");
+        number = Input(message);
+    }
+
+    // number = number > 0 ? number : Input(message);
+    return number;
+}
+
+double b1 = Input("Введите b1: ");
+double k1 = Input("Введите k1: ");
+double b2 = Input("Введите b2: ");
+double k2 = Input("Введите k2: ");
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Прямые пересекутся в точке\nX: {x:f2} | Y: {y:f2}");
